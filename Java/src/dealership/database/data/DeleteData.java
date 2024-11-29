2b067cbc2fc96fd85776cd28d880cf44a1a058d2
pbelem <pedro.belem@ucsal.edu.br>
2024-11-29 07:23:05 -0300

    Updating Readme

--- Java/src/dealership/database/data/DeleteData.java
+++ Java/src/dealership/database/data/DeleteData.java
@@ -7,15 +7,15 @@ import java.sql.Statement;
 public class DeleteData {
 
     public static void deleteAddress(int idAddress) {
-        executeDelete("DELETE FROM ADDRESS WHERE ID_ADDRESS = " + idAddress, "Endereço excluído com sucesso!");
+        executeDelete("DELETE FROM ADDRESS WHERE ID_ADDRESS = " + idAddress, "Address Successfully Deleted!");
     }
 
     public static void deleteCar(String chassi) {
-        executeDelete("DELETE FROM CAR WHERE CHASSI = '" + chassi + "'", "Carro excluído com sucesso!");
+        executeDelete("DELETE FROM CAR WHERE CHASSI = '" + chassi + "'", "Car Successfully Deleted!");
     }
 
     public static void deleteCustomer(String cpfCustomer) {
-        executeDelete("DELETE FROM CUSTOMER WHERE CPF_CUSTOMER = '" + cpfCustomer + "'", "Cliente excluído com sucesso!");
+        executeDelete("DELETE FROM CUSTOMER WHERE CPF_CUSTOMER = '" + cpfCustomer + "'", "Customer Successfully Deleted!");
     }
 
     public static void deleteCustomerCard(String cardNumber) {
@@ -23,27 +23,27 @@ public class DeleteData {
     }
 
     public static void deleteMechanic(String cpfMechanic) {
-        executeDelete("DELETE FROM MECHANIC WHERE CPF_MECHANIC = '" + cpfMechanic + "'", "Mecânico excluído com sucesso!");
+        executeDelete("DELETE FROM MECHANIC WHERE CPF_MECHANIC = '" + cpfMechanic + "'", "Mechanic Successfully Deleted!");
     }
 
     public static void deletePartCar(int serialNumber) {
-        executeDelete("DELETE FROM PART_CAR WHERE SERIAL_NUMBER = " + serialNumber, "Peça de carro excluída com sucesso!");
+        executeDelete("DELETE FROM PART_CAR WHERE SERIAL_NUMBER = " + serialNumber, "Car Part Successfully Deleted!");
     }
 
     public static void deleteSale(int idSale) {
-        executeDelete("DELETE FROM SALE WHERE ID_SALE = " + idSale, "Venda excluída com sucesso!");
+        executeDelete("DELETE FROM SALE WHERE ID_SALE = " + idSale, "Sale Successfully Deleted!");
     }
 
     public static void deleteSeller(String cpfSeller) {
-        executeDelete("DELETE FROM SELLER WHERE CPF_SELLER = '" + cpfSeller + "'", "Vendedor excluído com sucesso!");
+        executeDelete("DELETE FROM SELLER WHERE CPF_SELLER = '" + cpfSeller + "'", "Seller Successfully Deleted!");
     }
 
     public static void deleteService(int idService) {
-        executeDelete("DELETE FROM SERVICE WHERE ID_SERVICE = " + idService, "Serviço excluído com sucesso!");
+        executeDelete("DELETE FROM SERVICE WHERE ID_SERVICE = " + idService, "Seller Successfully Deleted!");
     }
 
     public static void deleteTypeSale(int idTypeSale) {
-        executeDelete("DELETE FROM TYPE_SALE WHERE ID_TYPE_SALE = " + idTypeSale, "Tipo de venda excluído com sucesso!");
+        executeDelete("DELETE FROM TYPE_SALE WHERE ID_TYPE_SALE = " + idTypeSale, "Type Sale Successfully Deleted!");
     }
 
     private static void executeDelete(String sql, String successMessage) {
